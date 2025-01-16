008797a43b931595be4006c6fe45afb7ea8db366
Jose Jimenez Caballero <143767138+JoseJimenezCaballero@users.noreply.github.com>
2025-01-16 01:05:08 -0500

    Updated parallel workers

--- python/server.py
+++ python/server.py
@@ -22,9 +22,9 @@ def getProfessorsAPI():
     unique_professor_names = [name for name in professorNames if name not in visited and not visited.add(name)] # Make a unique list of professor names to avoid duplicate requests
 
     # Use ThreadPoolExecutor for parallel processing
-    with ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(max_workers=20) as executor:# Trying to use 20 threads to get the data
         # Submit all tasks to the thread pool
-        future_to_name = {executor.submit(getProfessors, name): name for name in unique_professor_names}
+        future_to_name = {executor.submit(getProfessors, name): name for name in unique_professor_names}# This is needed because the future object does not have the name attribute so we keep track of it
 
         # Collect results as tasks complete
         for future in as_completed(future_to_name):
